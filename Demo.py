c2f2f495da854bc4b8aa614dbcb233e33b43218c
ImAbing <1@163.com>
2018-01-01 17:29:21 +0800

    create

---
+++ Demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from sklearn import datasets
+from sklearn import linear_model
+
+if __name__ == '__main__':
+    iris = datasets.load_iris()
+    print("The iris' target names: ", iris.target_names)
+    x = iris.data
+    y = iris.target
+
+    linear = linear_model.LinearRegression()
+    linear.fit(x, y)
+    print("linear's score: ", linear.score(x, y))
+    linear.coef_  # 系数
+    linear.intercept_  # 截距
+    print("predict: ", linear.predict([[7, 5, 2, 0.5], [7.5, 4, 7, 2]]))
