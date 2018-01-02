cdf904c04670a2b374e2dc87286fffb74a04b731
ImAbing <1@163.com>
2018-01-02 21:44:35 +0800

    iris data

--- Demo.py
+++ Demo.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import matplotlib.pyplot as plt
 import pandas as pd
+
+import pylab
+pylab.mpl.rcParams['font.sans-serif'] = ['SimHei']
+pylab.mpl.rcParams['axes.unicode_minus'] = False
 
 if __name__ == '__main__':
     iris = pd.read_csv("iris.csv")
@@ -9,7 +13,25 @@ if __name__ == '__main__':
     #打印数据集
     # print(iris)
     # 数据集列统计
-    print(iris.describe())
+    # print(iris.describe())
     # 鸢尾花类别:{'versicolor', 'virginica', 'setosa'}
     classes=set(iris.loc[:,'类别'])
     # print(classes)
+
+    #筛选setosa的数据，并作散点图
+    a = iris[iris['类别'] == 'setosa']
+    ax = a.plot(kind="scatter", x='萼长', y='萼宽', label='seotsa')
+
+    # 筛选versicolor的数据，并作散点图
+    b = iris[iris['类别'] == 'versicolor']
+    b.plot(kind="scatter", x='萼长', y='萼宽', color='red', label='versicolor', ax=ax)
+
+    # ＃筛选virginica的数据，并作散点图
+    c = iris[iris['类别'] == 'virginica']
+    c.plot(kind="scatter", x='萼长', y='萼宽', color='green', label="virginica", ax=ax)
+
+
+    plt.title('三个类别萼长与萼宽分布')
+    plt.legend(loc='upper right')
+    plt.show()
+
